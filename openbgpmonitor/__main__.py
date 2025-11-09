bd5dd86f4972532df5a15363958bc9a1e527df17
Julien cuvelier <juliencuvelier@users.noreply.github.com>
2025-11-09 18:13:36 +0100

    fix: Main File

--- openbgpmonitor/__main__.py
+++ openbgpmonitor/__main__.py
@@ -2,6 +2,10 @@ from openbgpmonitor.core.core_controller import CoreController
 from openbgpmonitor.services.bgp.bird.bird import BGPPybird
 
 
-def __main__():
+def main():
     core_controller = CoreController(bgp_service=BGPPybird())
     core_controller.run()
+
+
+if __name__ == "__main__":
+    main()
